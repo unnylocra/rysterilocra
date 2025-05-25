94b03a1444c5318559ac532c3da584a2267d8843
Max Nest <udomix.udomix@gmail.com>
2025-05-25 11:35:48 +0000

    troll skids

--- Client/Main.c
+++ Client/Main.c
@@ -63,9 +63,19 @@ static void *rr_create_game_thread(void *arg)
 
 #ifdef EMSCRIPTEN
 #include <emscripten.h>
+#include <Client/DOM.h>
+
+#define troll_skids                                                            \
+    if (!trusted)                                                              \
+    {                                                                          \
+        rr_page_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");           \
+        return;                                                                \
+    }
+
 void rr_key_event(struct rr_game *this, uint8_t type, uint32_t which,
-                  uint32_t key, uint8_t repeat)
+                  uint32_t key, uint8_t repeat, uint8_t trusted)
 {
+    troll_skids
     if (repeat)
         return;
     if (type == 1)
@@ -84,8 +94,9 @@ void rr_key_event(struct rr_game *this, uint8_t type, uint32_t which,
 }
 
 void rr_mouse_event(struct rr_game *this, float x, float y, uint8_t state,
-                    uint8_t button)
+                    uint8_t button, uint8_t trusted)
 {
+    troll_skids
     this->input_data->mouse_x = x;
     this->input_data->mouse_y = y;
     this->input_data->mouse_state = state;
@@ -102,8 +113,9 @@ void rr_mouse_event(struct rr_game *this, float x, float y, uint8_t state,
 }
 
 void rr_touch_event(struct rr_game *this, float x, float y, uint8_t state,
-                    uint8_t identifier)
+                    uint8_t identifier, uint8_t trusted)
 {
+    troll_skids
     struct rr_input_touch *touch = &this->input_data->touches[identifier % 16];
     touch->touch_x = x;
     touch->touch_y = y;
@@ -111,20 +123,26 @@ void rr_touch_event(struct rr_game *this, float x, float y, uint8_t state,
     touch->active = state;
 }
 
-void rr_wheel_event(struct rr_game *this, float delta)
+void rr_wheel_event(struct rr_game *this, float delta, uint8_t trusted)
 {
+    troll_skids
     this->input_data->scroll_delta = delta;
 }
 
-void rr_paste_event(struct rr_game *this, char *buf)
+void rr_paste_event(struct rr_game *this, char *buf, uint8_t trusted)
 {
+    if (!trusted)
+        free(buf);
+    troll_skids
     if (this->input_data->clipboard != NULL)
         free(this->input_data->clipboard);
     this->input_data->clipboard = buf;
 }
 
-void rr_context_event(struct rr_renderer *renderer, uint8_t type)
+void rr_context_event(struct rr_renderer *renderer, uint8_t type,
+                      uint8_t trusted)
 {
+    troll_skids
     if (type == 1 && renderer->on_context_restore != NULL)
         renderer->on_context_restore(renderer->on_context_restore_captures);
 }
@@ -135,8 +153,9 @@ static void release_key(uint64_t i, void *captures)
     rr_bitset_set(this->input_data->keys_released_this_tick, i);
 }
 
-void rr_focus_event(struct rr_game *this, uint8_t type)
+void rr_focus_event(struct rr_game *this, uint8_t type, uint8_t trusted)
 {
+    troll_skids
     if (type == 1)
     {
         this->input_data->mouse_buttons_up_this_tick |=
@@ -149,6 +168,7 @@ void rr_focus_event(struct rr_game *this, uint8_t type)
         memset(this->input_data->keys_pressed, 0, RR_BITSET_ROUND(256));
     }
 }
+#undef troll_skids
 #else
 #endif
 
@@ -173,7 +193,7 @@ void rr_main_loop(struct rr_game *this)
                     $0, 1, e.which,
                     e.key ? (!e.ctrlKey && !e.metaKey && e.key.length == 1) *
                                 e.key.charCodeAt()
-                          : 0, +e.repeat);
+                          : 0, +e.repeat, +e.isTrusted);
             };
             window.onkeyup = function(e)
             {
@@ -181,7 +201,7 @@ void rr_main_loop(struct rr_game *this)
                     $0, 0, e.which,
                     e.key ? (!e.ctrlKey && !e.metaKey && e.key.length == 1) *
                                 e.key.charCodeAt()
-                          : 0, +e.repeat);
+                          : 0, +e.repeat, +e.isTrusted);
             };
             window.addEventListener(
                 "mousedown", async function(e) {
@@ -189,7 +209,8 @@ void rr_main_loop(struct rr_game *this)
                     const clientY = e.clientY;
                     const button = e.button;
                     _rr_mouse_event($0, clientX * devicePixelRatio,
-                                    clientY * devicePixelRatio, 1, +!!button);
+                                    clientY * devicePixelRatio, 1, +!!button,
+                                    +e.isTrusted);
                 });
             window.addEventListener(
                 "mousemove", async function(e) {
@@ -197,7 +218,8 @@ void rr_main_loop(struct rr_game *this)
                     const clientY = e.clientY;
                     const button = e.button;
                     _rr_mouse_event($0, clientX * devicePixelRatio,
-                                    clientY * devicePixelRatio, 2, +!!button);
+                                    clientY * devicePixelRatio, 2, +!!button,
+                                    +e.isTrusted);
                 });
             window.addEventListener(
                 "mouseup", async function(e) {
@@ -205,7 +227,8 @@ void rr_main_loop(struct rr_game *this)
                     const clientY = e.clientY;
                     const button = e.button;
                     _rr_mouse_event($0, clientX * devicePixelRatio,
-                                    clientY * devicePixelRatio, 0, +!!button);
+                                    clientY * devicePixelRatio, 0, +!!button,
+                                    +e.isTrusted);
                 });
             window.addEventListener(
                 "touchstart",
@@ -216,11 +239,12 @@ void rr_main_loop(struct rr_game *this)
                         return;
                     const touch = e.changedTouches[0];
                     _rr_mouse_event($0, touch.clientX * devicePixelRatio,
-                                    touch.clientY * devicePixelRatio, 1, 0);
+                                    touch.clientY * devicePixelRatio, 1, 0,
+                                    +e.isTrusted);
                     for (const t of e.changedTouches)
                         _rr_touch_event($0, t.clientX * devicePixelRatio,
                                         t.clientY * devicePixelRatio, 1,
-                                        t.identifier);
+                                        t.identifier, +e.isTrusted);
                 },
                 {passive : false});
             window.addEventListener(
@@ -232,11 +256,12 @@ void rr_main_loop(struct rr_game *this)
                         return;
                     const touch = e.changedTouches[0];
                     _rr_mouse_event($0, touch.clientX * devicePixelRatio,
-                                    touch.clientY * devicePixelRatio, 2, 0);
+                                    touch.clientY * devicePixelRatio, 2, 0,
+                                    +e.isTrusted);
                     for (const t of e.changedTouches)
                         _rr_touch_event($0, t.clientX * devicePixelRatio,
                                         t.clientY * devicePixelRatio, 1,
-                                        t.identifier);
+                                        t.identifier, +e.isTrusted);
                 },
                 {passive : false});
             window.addEventListener(
@@ -248,16 +273,19 @@ void rr_main_loop(struct rr_game *this)
                         return;
                     const touch = e.changedTouches[0];
                     _rr_mouse_event($0, touch.clientX * devicePixelRatio,
-                                    touch.clientY * devicePixelRatio, 0, 0);
+                                    touch.clientY * devicePixelRatio, 0, 0,
+                                    +e.isTrusted);
                     for (const t of e.changedTouches)
                         _rr_touch_event($0, t.clientX * devicePixelRatio,
                                         t.clientY * devicePixelRatio, 0,
-                                        t.identifier);
+                                        t.identifier, +e.isTrusted);
                 },
                 {passive : false});
             window.addEventListener(
                 "wheel",
-                async function({deltaY}) { _rr_wheel_event($0, deltaY); });
+                async function(e) {
+                    _rr_wheel_event($0, e.deltaY, +e.isTrusted);
+                });
             document.body.addEventListener(
                 "paste", async function(e) {
                     const buf = new TextEncoder().encode(
@@ -265,10 +293,10 @@ void rr_main_loop(struct rr_game *this)
                     const $a = _malloc(buf.length + 1);
                     HEAPU8.set(buf, $a);
                     HEAPU8[$a + buf.length] = 0;
-                    _rr_paste_event($0, $a);
+                    _rr_paste_event($0, $a, +e.isTrusted);
                 });
             window.addEventListener("blur", function(e) {
-                _rr_focus_event($0, 1);
+                _rr_focus_event($0, 1, +e.isTrusted);
             });
             Module.addCtx = function(renderer)
             {
@@ -279,8 +307,8 @@ void rr_main_loop(struct rr_game *this)
                         return 0; // used for the main ctx, because that has
                                   // special behavior
                     const ocanvas = new OffscreenCanvas(1, 1);
-                    ocanvas.addEventListener("contextrestored", function() {
-                        _rr_context_event(renderer, 1);
+                    ocanvas.addEventListener("contextrestored", function(e) {
+                        _rr_context_event(renderer, 1, +e.isTrusted);
                     });
                     Module.ctxs[index] = ocanvas.getContext('2d');
                     return index;
